d3cccefb3d525ce440ca6343d4d639e572dec430
Dreamwalker <aristojeff@gmail.com>
2021-07-25 09:44:12 +0900

    [Add] server code

---
+++ server/ex1/myapp/ex29_5_test.go
@@ -0,0 +1,38 @@
+package myapp
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIndexHandler(t *testing.T) {
+	assert := assert.New(t)
+
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/", nil)
+
+	mux := MakeWebHandler()
+	mux.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusOK, res.Code)
+	data, _ := io.ReadAll(res.Body)
+	assert.Equal("Hello World", string(data))
+}
+
+func TesBarHandler(t *testing.T) {
+	assert := assert.New(t)
+
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/bar", nil) // ➎ /bar 경로 테스트
+
+	mux := MakeWebHandler()
+	mux.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusOK, res.Code)
+	data, _ := io.ReadAll(res.Body)
+	assert.Equal("Hello Bar", string(data))
+}
